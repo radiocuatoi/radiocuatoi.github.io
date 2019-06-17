35d552c844903654cb11db7ab2b16c11273af0cc
dungviettran89 <dungviettran89@gmail.com>
2019-06-17 21:34:21 +0800

    Loaded external css from javascript

--- micro-angular/src/application.js
+++ micro-angular/src/application.js
@@ -4,6 +4,17 @@ import "angular-messages";
 import "angular-animate";
 import "angular-aria";
 
+export function loadStyles(url) {
+    let link = document.createElement("link");
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+loadStyles('https://fonts.googleapis.com/css?family=Roboto:300,400,500');
+loadStyles('https://fonts.googleapis.com/icon?family=Material+Icons');
+loadStyles('https://ajax.googleapis.com/ajax/libs/angular_material/1.1.19/angular-material.min.css');
+
 export const application = angular.module("Application", [
     "ngMaterial",
     "ngMessages"
--- micro-angular/src/components/home-page.js
+++ micro-angular/src/components/home-page.js
@@ -24,6 +24,27 @@ export class HomePage {
 application.component('homePage', {
     bindings: {state: '<'},
     template: `
+<md-toolbar class="app-toolbar md-hue-2">
+    <div class="md-toolbar-tools">
+        <md-button class="md-icon-button" aria-label="Settings">
+            <md-icon md-font-icon="material-icons">menu</md-icon>
+        </md-button>
+
+        <h2 flex md-truncate>{{ $ctrl.name }} Menu</h2>
+
+        <md-button class="md-icon-button" aria-label="Settings" ng-disabled="true">
+            <md-icon md-font-icon="material-icons" >settings</md-icon>
+        </md-button> 
+        
+         <md-button class="md-icon-button" aria-label="Events"  ng-disabled="true">
+            <md-icon md-font-icon="material-icons" >notifications</md-icon>
+        </md-button> 
+
+        <md-button class="md-icon-button" aria-label="Sign Out">
+            <md-icon md-font-icon="material-icons">exit_to_app</md-icon>
+        </md-button>
+    </div>
+</md-toolbar> 
 <h3 class="name">Hello {{ $ctrl.name }}.</h3>  
     `,
     controller: HomePage
